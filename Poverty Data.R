70e77c41005e3cc8ec54ef161c12695d449f8c74
Anna Cook <annacook@bu.edu>
2020-12-10 11:37:30 -0500

    final draft

--- Poverty Data.R
+++ Poverty Data.R
@@ -126,11 +126,13 @@ data_merge <- left_join(poverty_final, elections, by = c("state", "year"))
 ## MERGED DATA EDA
 
 # summarize across years
-group_by_state <- data_merge %>% group_by(state, race_ethnicity) %>% summarize(poverty_rate = mean(poverty_rate))
+group_by_state <- subset(data_merge, party == "republican") %>% group_by(state, year) %>% summarize(poverty_rate = mean(poverty_rate), vote_pct = mean(vote_pct))
 
-# povert
-ggplot() + geom_point(data = group_by_state, aes(x = race_ethnicity, y = poverty_rate, color = state))
+# poverty rate vs. republican vote share
+ggplot() + geom_point(data = group_by_state, aes(x = poverty_rate, y = vote_pct, color = factor(year))) +
+  ggtitle("Poverty Rate vs. Vote Share for Republican Candidate") + xlab("Poverty Rate") + ylab("Vote Share %")
 
+## Plots tried out but not informative for research question--ignore these ##
 # summarize across states
 group_by_race <- data_merge %>% group_by(race_ethnicity, party, year) %>% summarize(poverty_rate = mean(poverty_rate), vote_pct = mean(vote_pct))
 
@@ -139,6 +141,18 @@ ggplot() + geom_point(data = subset(group_by_race, year == 2012), aes(x = race_e
 
 ggplot() + geom_point(data = data_merge, aes(x = year, y = vote_pct, color = party)) + 
   facet_wrap(vars(data_merge$race_ethnicity))
+
+ggplot() + geom_point(data = subset(data_merge, party == "republican"), aes(x = poverty_rate, y = vote_pct, color = race_ethnicity)) +
+  ggtitle("Poverty Rate vs. Vote Share for Republican Candidate") + 
+  ylab("Vote Share %") + facet_wrap(vars(race_ethnicity)) +
+  scale_color_discrete(name = "Race/Ethnicity", labels = c("Native Am.", "Asian", "Black", "Hispanic", "Total", "White"))
+
+ggplot() + geom_point(data = subset(data_merge, party == "republican"), aes(x = poverty_rate, y = vote_pct, color = race_ethnicity)) +
+  ggtitle("Poverty Rate vs. Vote Share for Republican Candidate") + 
+  ylab("Vote Share %") +
+  scale_color_discrete(name = "Race/Ethnicity", labels = c("Native Am.", "Asian", "Black", "Hispanic", "Total", "White"))
+
+
 
 ## MODELING
 
@@ -155,23 +169,47 @@ fit3 <- stan_glmer(poverty_rate ~ race_ethnicity + (1|year), data = data_merge)
 pp_check(fit3)
 plot(fitted(fit3), resid(fit3))
 
-fit4 <- stan_glm(vote_pct ~ race_ethnicity + state, data = subset(data_merge, party = "republican"), refresh = 0)
+fit4 <- stan_glm(vote_pct ~ race_ethnicity + state, data = subset(data_merge, party == "republican"), refresh = 0)
 pp_check(fit4)
 plot(fitted(fit4), resid(fit4))
 
-fit5 <- stan_glmer(poverty_rate ~ race_ethnicity + (1|state), data = data_merge)
-print(fit5)
-summary(fit5)
-pp_check(fit5)
-plot(fitted(fit5), resid(fit5))
+# final model1
+model1 <- stan_glmer(poverty_rate ~ race_ethnicity + (1|state), data = data_merge, refresh = 0)
+print(model1)
+fixef(model1)
+ranef(model1)
+pp_check(model1)
+plot(fitted(model1), resid(model1), xlab = "Fitted Values", ylab = "Residuals", main = "Residual Plot for Model 1" )
 
 fit6 <- lmer(poverty_rate ~ race_ethnicity + (1|state), data = data_merge)
 print(fit6)
 
-fit7 <- stan_glmer(vote_pct ~ race_ethnicity + (1|state), data = subset(data_merge, party = "republican"), refresh = 0)
+fit7 <- stan_glmer(vote_pct ~ race_ethnicity + (1|state), data = subset(data_merge, party == "republican"), refresh = 0)
 fit7
 pp_check(fit7)
 plot(fitted(fit7), resid(fit7))
+
+fit8 <- stan_glmer(vote_pct ~ poverty_rate + (1|state), data = subset(data_merge, party == "republican"))
+fit8
+pp_check(fit8)
+plot(fitted(fit8), resid(fit8))
+
+fit9 <- stan_glm(vote_pct ~ poverty_rate + state, data = subset(data_merge, party == "republican"))
+fit9
+pp_check(fit9)
+plot(fitted(fit9), resid(fit9))
+
+#final model2
+model2 <- stan_glm(vote_pct ~ poverty_rate, data = group_by_state, refresh = 0)
+print(model2)
+pp_check(model2)
+plot(fitted(model2), resid(model2), xlab = "Fitted Values", ylab = "Residuals", main = "Residual Plot for Model 2")
+
+fit11 <- stan_glmer(vote_pct ~ poverty_rate + (1 + poverty_rate |state), data = group_by_state)
+fit11
+summary(fit11)
+pp_check(fit11)
+plot(fitted(fit11), resid(fit11))
 
 ## CITATIONS
 
